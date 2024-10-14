e65c4220d7986dc89961e07a496774625344717a
1980mmaria <artemiotantaricocueva@gmail.com>
2024-10-14 17:04:13 -0500

    enlases

--- Logic/conexion.php
+++ Logic/conexion.php
@@ -7,6 +7,8 @@ $db = "bd_ventas";
 //creamos las conexiones
 try {
     $conexion = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $user, $pass);
+    $conexion ->setAttribute(PDO:: ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    echo "conectado";
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
